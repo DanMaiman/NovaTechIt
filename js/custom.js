446388c02a4c7dcb15355b55280f51ac93df6c9d
tallytutor <Dan@Daniels-MacBook-Air.local>
2016-03-09 20:39:06 -0500

    Added slides to homepage

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,31 @@
 jQuery( document ).ready(function($) {
+    var scrollIndex = 0;
+    $('#img-1').addClass('fadeIn');
 
     $('#fullpage').fullpage({
+        responsiveWidth: 767,
+
+        afterLoad: function(anchorLink, index){
+            var loadedSection = $(this);
+
+            //using index
+            console.log(index);
+            if (index == 2 && $('#img-2').hasClass('fadeIn')) {
+                $('#img-2').removeClass('fadeIn').addClass('fadeOut');
+            }
+
+            if (index == 3) {
+                $('#img-2').addClass('fadeIn').removeClass('fadeOut');
+                if($('#img-3').hasClass('fadeIn')) {
+                    $('#img-3').removeClass('fadeIn').addClass('fadeOut');
+                }
+            }
+
+            if (index == 4) {
+                $('#img-3').addClass('fadeIn').removeClass('fadeOut');
+                $('#img-4').removeClass('fadeIn').addClass('fadeOut');
+            }
+        }
     });
 
 });
