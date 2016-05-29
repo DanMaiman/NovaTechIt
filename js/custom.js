cef7c18cdd7b79ac608b3dedbb37ddcb4da23288
tallytutor <Dan@daniels-air.attlocal.net>
2016-05-29 17:57:41 -0400

    unify image changing implementation

--- js/custom.js
+++ js/custom.js
@@ -83,7 +83,7 @@ jQuery(document).ready(function($) {
             var indexMinus = index - 1;
             console.log (index);
 
-            if ($(window).width() > 767) {
+           // if ($(window).width() > 767) {
                 //using index
                 if (index > 1 && index < 6) {
                     $('img[data-img="' + index + '" ]').removeClass('fadeOut').addClass('fadeIn');
@@ -95,7 +95,7 @@ jQuery(document).ready(function($) {
                     $('img[data-img!="5" ]').removeClass('fadeIn').addClass('fadeOut');
                 }
 
-            }
+          //  }
             /*else {
                            if ($('.feat[data-index="' + index + '" ]').hasClass('fadeOutUp')) {
                                $('.feat[data-index="' + index + '" ]').addClass('animated fadeInDown').removeClass('init fadeOutUp');
@@ -114,38 +114,38 @@ jQuery(document).ready(function($) {
     var windowHeight = $(window).height();
 
 
-    if ($(window).width() < 768) {
-        var twoOffset = $("#two").offset().top;
-        var threeOffset = $("#three").offset().top;
-        var fourOffset = $("#four").offset().top;
+    // if ($(window).width() < 768) {
+    //     var twoOffset = $("#two").offset().top;
+    //     var threeOffset = $("#three").offset().top;
+    //     var fourOffset = $("#four").offset().top;
 
-        $(window).scroll(function() {
-            var scrollPos = $(window).scrollTop();
-            var scrollBot = scrollPos - 0.4 * windowHeight;
+    //     $(window).scroll(function() {
+    //         var scrollPos = $(window).scrollTop();
+    //         var scrollBot = scrollPos - 0.4 * windowHeight;
 
-            if (scrollBot <= twoOffset && $('#img-2').hasClass('fadeIn')) {
-                $('#img-2').removeClass('fadeIn').addClass('fadeOut');
-            }
+    //         if (scrollBot <= twoOffset && $('#img-2').hasClass('fadeIn')) {
+    //             $('#img-2').removeClass('fadeIn').addClass('fadeOut');
+    //         }
 
-            if (scrollBot > twoOffset) {
-                $('#img-2').addClass('fadeIn').removeClass('fadeOut');
-                if ($('#img-3').hasClass('fadeIn')) {
-                    $('#img-3').removeClass('fadeIn').addClass('fadeOut');
-                }
-            }
+    //         if (scrollBot > twoOffset) {
+    //             $('#img-2').addClass('fadeIn').removeClass('fadeOut');
+    //             if ($('#img-3').hasClass('fadeIn')) {
+    //                 $('#img-3').removeClass('fadeIn').addClass('fadeOut');
+    //             }
+    //         }
 
-            if (scrollBot > threeOffset) {
-                $('#img-3').addClass('fadeIn').removeClass('fadeOut');
-                if ($('#img-4').hasClass('fadeIn')) {
-                    $('#img-4').removeClass('fadeIn').addClass('fadeOut');
-                }
-            }
+    //         if (scrollBot > threeOffset) {
+    //             $('#img-3').addClass('fadeIn').removeClass('fadeOut');
+    //             if ($('#img-4').hasClass('fadeIn')) {
+    //                 $('#img-4').removeClass('fadeIn').addClass('fadeOut');
+    //             }
+    //         }
 
-            if (scrollBot > fourOffset) {
-                $('#img-4').addClass('fadeIn').removeClass('fadeOut');
-            }
-        });
-    }
+    //         if (scrollBot > fourOffset) {
+    //             $('#img-4').addClass('fadeIn').removeClass('fadeOut');
+    //         }
+    //     });
+    // }
 
     /*$(window).scroll(function () {
         var scrollPos = $(window).scrollTop();
